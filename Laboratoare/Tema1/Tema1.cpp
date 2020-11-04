f6181cee1802ad6c2d4a343ef488bed8ba2f6fd7
andreea-h <59268044+andreea-h@users.noreply.github.com>
2020-11-04 03:04:54 +0200

    Add files via upload
    adauga animatie baloane

--- Laboratoare/Tema1/Tema1.cpp
+++ Laboratoare/Tema1/Tema1.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <iostream>
 #include <Core/Engine.h>
+#include <cmath>
+#include <glm/gtx/string_cast.hpp> //afisare matrice
 
 using namespace std;
 
@@ -24,25 +26,68 @@ void Tema1::Init() {
 	camera->Update();
 	GetCameraInput()->SetActive(false);
 
-	Mesh* shuriken = CreateShuriken();
+	Mesh* shuriken = CreateShuriken(30);
 	AddMeshToList(shuriken);
+
+	Mesh* arrow = CreateArrow(5, 20);
+	AddMeshToList(arrow);
+
+	Mesh* bow = CreateBow();
+	AddMeshToList(bow);
+
+	Mesh* yellowBallon = CreateBallon('y', 30);
+	AddMeshToList(yellowBallon);
+
+	Mesh* redBallon = CreateBallon('r', 30);
+	AddMeshToList(redBallon);
+
+	int i;
+	for (i = 0; i < 10; i++) {
+		translateY_values.push_back(0 - 200 * i);
+	}
 }
 
 
-Mesh* Tema1::CreateShuriken() {
+Mesh* Tema1::CreateArrow(float arrowSide, float lengthFactor) {
 
 	glm::vec3 color = glm::vec3(1.000, 0.078, 0.576);
 	vector<VertexFormat> vertices =
 	{
 		VertexFormat(glm::vec3(0, 0, 0), color),
-		VertexFormat(glm::vec3(-50, 50, 0), color),
-		VertexFormat(glm::vec3(0, 50, 0), color),
-		VertexFormat(glm::vec3(50, 50, 0), color),
-		VertexFormat(glm::vec3(50, 0, 0), color),
-		VertexFormat(glm::vec3(50, -50, 0), color),
-		VertexFormat(glm::vec3(0, -50, 0), color),
-		VertexFormat(glm::vec3(-50, -50, 0), color),
-		VertexFormat(glm::vec3(-50, 0, 0), color),
+		VertexFormat(glm::vec3(0, -arrowSide, 0), color), 
+		VertexFormat(glm::vec3(lengthFactor * arrowSide, 0, 0), color),
+		VertexFormat(glm::vec3(lengthFactor * arrowSide, -arrowSide, 0), color),
+		VertexFormat(glm::vec3(lengthFactor * arrowSide, 2 * arrowSide, 0), color),
+		VertexFormat(glm::vec3((lengthFactor + 3) * arrowSide, -arrowSide / 2, 0), color),
+		VertexFormat(glm::vec3(lengthFactor * arrowSide, - 3 * arrowSide, 0), color)
+	};
+
+	vector<unsigned short> indices = {
+		0, 1, 2,
+		1, 3, 2,
+		4, 6, 5
+	};
+
+	arrowLength = lengthFactor * arrowSide + 3 * arrowSide;
+	Mesh* arrow = new Mesh("arrow");
+	arrow->InitFromData(vertices, indices);
+	return arrow;
+}
+
+Mesh* Tema1::CreateShuriken(float shurikenSide) {
+
+	glm::vec3 color = glm::vec3(1.000, 0.078, 0.576);
+	vector<VertexFormat> vertices =
+	{
+		VertexFormat(glm::vec3(0, 0, 0), color),
+		VertexFormat(glm::vec3(-1, 1, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(0, 1, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(1, 1, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(1, 0, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(1, -1, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(0, -1, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(-1, -1, 0) * shurikenSide, color),
+		VertexFormat(glm::vec3(-1, 0, 0) * shurikenSide, color),
 	};
 
 	vector<unsigned short> indices = {
@@ -57,14 +102,219 @@ Mesh* Tema1::CreateShuriken() {
 	return shuriken;
 }
 
+Mesh* Tema1::CreateBow() {
+	glm::vec3 color = glm::vec3(1.000, 0.078, 0.576);
+	float radius = 100;
+	vector<VertexFormat> vertices;
+	float i, j;
+	int step1 = 0;
+	for (i = 0.4; i <= M_PI - 0.4; i += 0.001) {
+		vertices.push_back(VertexFormat(glm::vec3(radius * sinf(i), radius * cosf(i), 0), color));
+		step1++;
+	}
+
+	vector<unsigned short> indices;
+	int k;
+	
+	for (k = 0; k < step1; k++) {
+		indices.push_back(k);
+	}
+
+	Mesh* bow = new Mesh("bow");
+	bow->InitFromData(vertices, indices);
+	bow->SetDrawMode(GL_LINE_LOOP);
+	return bow;
+}
+
+Mesh* Tema1::CreateBallon(char color, float R) {
+	glm::vec3 color1 = glm::vec3(0, 0, 0);
+	if (color == 'r') {
+		color1 = glm::vec3(1, 0, 0); //rosu
+	}
+	if (color == 'y') {
+		color1 = glm::vec3(1, 1, 0); //galben
+	}
+	vector<VertexFormat> vertices;
+	vector<unsigned short> indices;
+	float i;
+	int step = 0;
+	vertices.push_back(VertexFormat(glm::vec3(0, 0, 0), color1)); //centrul cercului care va reprezenta balonul
+	for (i = 0; i <= 2 * M_PI; i += 0.001) {
+		vertices.push_back(VertexFormat(glm::vec3(R * sinf(i), R * cosf(i), 0), color1));
+		step++;
+	}
+	//adauga punctele din partea de jos a balonului (triunghiul mic)
+	vertices.push_back(VertexFormat(glm::vec3(0, -R + 4, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-10, -R - 10, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(10, -R - 10, 0), color1));
+
+	//polilinia
+	vertices.push_back(VertexFormat(glm::vec3(2, -R -10, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(0, -R -9, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R -17, 0), color1));
+	
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 17, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-3, -R - 19, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(2, -R - 10, 0), color1));
+
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 17, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(3, -R -21, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(2, -R - 23, 0), color1));
+
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 17, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(3, -R - 21, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-3, -R - 15, 0), color1));
+
+	vertices.push_back(VertexFormat(glm::vec3(3, -R - 21, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(2, -R - 19, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 26, 0), color1));
+
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 26, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-3, -R - 29, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(3, -R - 21, 0), color1));
+
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 26, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(3, -R - 33, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(5, -R - 30, 0), color1));
+
+	vertices.push_back(VertexFormat(glm::vec3(-5, -R - 26, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(5, -R - 30, 0), color1));
+	vertices.push_back(VertexFormat(glm::vec3(-3, -R - 24, 0), color1));
+
+	int j;
+	for (j = 1; j < step; j++) {
+		indices.push_back(j);
+		indices.push_back(j - 1);
+		indices.push_back(0); //"uneste" de centru 
+	}
+	for (j = step + 1; j <= step + 27; j++) {
+		indices.push_back(j);
+	}
+	
+	if (color == 'y') {
+		Mesh* yellowBallon = new Mesh("yellowBallon");
+		yellowBallon->InitFromData(vertices, indices);
+		return yellowBallon;
+	}
+	if (color == 'r') {
+		Mesh* redBallon = new Mesh("redBallon");
+		redBallon->InitFromData(vertices, indices);
+		return redBallon;
+	}
+}
+
 void Tema1::Update(float deltaTimeSeconds) {
-	modelMatrix = glm::mat3(1);
-	modelMatrix *= Transform2D::Translate(150, 250);
-	RenderMesh2D(meshes["shuriken"], shaders["VertexColor"], modelMatrix);
+	int i;
+	for (i = 0; i < 10; i++) {
+		translateY_values[i] += 250 * deltaTimeSeconds;
+		if (translateY_values[i] > window->GetResolution().y + 600) {
+			translateY_values[i] = - 1200;
+		}
+	}
+	modelMatrixArrow = glm::mat3(1);
+	modelMatrixArrow *= Transform2D::Translate(100, 250 + translateY);
+	modelMatrixArrow *= Transform2D::Rotate(radiusArrow);
+	modelMatrixArrow *= Transform2D::Scale(2.0f, 2.0f);
+	
+	//recalculeaza coordonatele varfului sagetii
+	arrowTop = glm::vec3(arrowLength + modelMatrixArrow[2][0], 720 - modelMatrixArrow[2][1], 0);
+	RenderMesh2D(meshes["arrow"], shaders["VertexColor"], modelMatrixArrow);
+
+	modelMatrixBow = glm::mat3(1);
+	modelMatrixBow *= Transform2D::Translate(50, 250 + translateY);
+	modelMatrixBow *= Transform2D::Rotate(radiusArrow);
+	modelMatrixBow *= Transform2D::Scale(2.0f, 2.0f);
+	RenderMesh2D(meshes["bow"], shaders["VertexColor"], modelMatrixBow);
+
+	modelMatrixShuriken = glm::mat3(1);
+	modelMatrixShuriken *= Transform2D::Translate(800, 250);
+	RenderMesh2D(meshes["shuriken"], shaders["VertexColor"], modelMatrixShuriken);
+	//////////////////////////////////////////////////////////////////////////////
+	
+	for (i = 0; i < 10; i++) {
+		
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1000, translateY_values[i]);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		if (i % 2 == 0) {
+			RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+		}
+		else {
+			RenderMesh2D(meshes["redBallon"], shaders["VertexColor"], modelMatrixBallonY);
+		}
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(800, translateY_values[i] - 225);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		if (i % 2 == 0) {
+			RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+		}
+		else {
+			RenderMesh2D(meshes["redBallon"], shaders["VertexColor"], modelMatrixBallonY);
+		}
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1200, translateY_values[i] - 450);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+	}
+
+	/*
+		//se calculeaza random distantele dintre baloane
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1200, moveBallon);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1000, moveBallon);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1000, moveBallon - 300);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["redBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1200, moveBallon - 200);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1100, moveBallon - 100);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["redBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(900, moveBallon - 400);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["redBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1000, moveBallon - 500);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1250, moveBallon - 600);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+
+		modelMatrixBallonY = glm::mat3(1);
+		modelMatrixBallonY *= Transform2D::Translate(1000, moveBallon - 700);
+		modelMatrixBallonY *= Transform2D::Scale(1, 1.25f);
+		RenderMesh2D(meshes["yellowBallon"], shaders["VertexColor"], modelMatrixBallonY);
+*/
+	/*modelMatrixBallonR = glm::mat3(1);
+	modelMatrixBallonR *= Transform2D::Translate(1200, 250);
+	modelMatrixBallonR *= Transform2D::Scale(1, 1.25f);
+	modelMatrixBallonR *= Transform2D::Scale(0.85f, 0.85f);
+	RenderMesh2D(meshes["redBallon"], shaders["VertexColor"], modelMatrixBallonR);*/
+	
 }
 
 void Tema1::FrameStart() {
-	
 	glClearColor(0.196, 0.804, 0.196, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -78,6 +328,13 @@ void Tema1::FrameEnd() {
 
 void Tema1::OnInputUpdate(float deltaTime, int mods)
 {
+	if (window->KeyHold(GLFW_KEY_S)) {
+		translateY -= deltaTime * 100;
+	}
+
+	if (window->KeyHold(GLFW_KEY_W)) {
+		translateY += deltaTime * 100;
+	}
 }
 
 void Tema1::OnKeyPress(int key, int mods)
@@ -90,6 +347,13 @@ void Tema1::OnKeyRelease(int key, int mods)
 
 void Tema1::OnMouseMove(int mouseX, int mouseY, int deltaX, int deltaY)
 {
+	//cout<<glm::to_string(modelMatrixArrow)<<std::endl;
+	//calculeaza distanta de la mouse la varful sagetii
+	float diffX = mouseX - arrowTop[0];
+	float diffY = mouseY - arrowTop[1];
+	radiusArrow = -atan(diffY / diffX);
+	//cout << mouseX << " " << mouseY <<"\n";
+	//cout << "delta" << deltaX <<"//" << deltaY << "\n";
 }
 
 void Tema1::OnMouseBtnPress(int mouseX, int mouseY, int button, int mods)
--- Laboratoare/Tema1/Tema1.h
+++ Laboratoare/Tema1/Tema1.h
@@ -3,6 +3,7 @@
 #include <Component/SimpleScene.h>
 #include <string>
 #include <Core/Engine.h>
+#include <vector>
 
 class Tema1 : public SimpleScene {
 	public:
@@ -16,9 +17,11 @@ class Tema1 : public SimpleScene {
 		void Update(float deltaTimeSeconds) override;
 		void FrameEnd() override;
 
-		Mesh* CreateMesh(const char* name, const std::vector<VertexFormat>& vertices, const std::vector<unsigned short>& indices);
-		Mesh* CreateShuriken();
-		
+		Mesh* CreateShuriken(float shurikenSide);
+		Mesh* CreateArrow(float arrowSide, float lengthFactor);
+		Mesh* CreateBow();
+		Mesh* CreateBallon(char color, float radius); //culoare = y/r, radius = raza inainte de deformare balon
+
 		void OnInputUpdate(float deltaTime, int mods) override;
 		void OnKeyPress(int key, int mods) override;
 		void OnKeyRelease(int key, int mods) override;
@@ -28,6 +31,23 @@ class Tema1 : public SimpleScene {
 		void OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) override;
 		void OnWindowResize(int width, int height) override;
 
+
 	protected:
-		glm::mat3 modelMatrix;
+		glm::mat3 modelMatrixShuriken;
+		glm::mat3 modelMatrixArrow;
+		glm::mat3 modelMatrixBow;
+		glm::mat3 modelMatrixBallonY;
+		glm::mat3 modelMatrixBallonR;
+		float shurikenSide;
+		float arrowSide;
+			
+		float radiusArrow = 0;
+		float arrowLength; //lungimea sagetii
+		glm::vec3 arrowTop; //coordonatele varfului sagetii
+
+		float translateY; //deplasarea arcului pe directie verticala
+		bool w_pressed = false; //true daca s-a apasat
+		bool s_pressed = false;
+		float moveBallon;
+		std::vector<float> translateY_values = {}; //valorile translatiilor pe oY ale baloanelor
 };
